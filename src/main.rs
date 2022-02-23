d94508f3f09bfd41338acc38b8fe730d6316ad45
rmctJ <11417719+rmctJ@users.noreply.github.com>
2022-02-23 11:43:15 +0900

    Demo of actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use chrono::Local;
+use std::io::Result;
+
+#[get("/")]
+async fn index_root() -> impl Responder {
+    let time = Local::now();
+    format!("Hello from demo-actix_web, time:{}", time.to_rfc3339())
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let bind_addr = "0.0.0.0:2001";
+
+    let r = HttpServer::new(|| App::new().service(index_root))
+        .bind(bind_addr)?
+        .run();
+
+    println!("bind to {}", bind_addr);
+    r.await
 }
